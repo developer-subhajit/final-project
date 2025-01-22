30d6a63e8967dbdbd258a86943bc15ec8853fa5e
developer-subhajit <subhajitbhar1@gmail.com>
2025-01-22 10:04:57 -0500

    added new test

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,14 +7,33 @@ def emotion_detector(text_to_analyze):
     input_json = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, json=input_json, headers=headers)
     
-    # Convert the response text into a dictionary using the json library functions.
-    formatted_response = json.loads(response.text)
+    # Get the status code
+    status_code = response.status_code
 
-    # Extract the required set of emotions, including anger, disgust, fear, joy and sadness, 
-    # along with their scores.
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    emotions = {}
 
-    # Extract the dominant emotion from the response
-    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    if status_code == 400:
+        #  For status_code = 400, make the function return the same dictionary,
+        #  but with values for all keys being None.
+        emotions = {
+            'anger': None ,
+            'disgust': None, 
+            'fear': None, 
+            'joy':  None, 
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    elif status_code == 200:
+
+        # Convert the response text into a dictionary using the json library functions.
+        formatted_response = json.loads(response.text)
+
+        # Extract the required set of emotions, including anger, disgust, fear, joy and sadness, 
+        # along with their scores.
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+        # Extract the dominant emotion from the response
+        emotions['dominant_emotion'] = max(emotions, key=emotions.get)
 
     return emotions
