dcc8282853b41de4fd8b02606e7f69aa33e1c401
developer-subhajit <subhajitbhar1@gmail.com>
2025-01-22 10:25:54 -0500

    added new style

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+This module contains the Flask application for the emotion detection service.
+"""
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,26 +9,27 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detector_route():
+    """
+    Handles the emotion detection route.
+    """
     text_to_analyze = request.args.get("textToAnalyze")
-    print(text_to_analyze)
     emotions = emotion_detector(text_to_analyze)
 
     if emotions.get("dominant_emotion") is None:
         return "Invalid text! Please try again"
 
-    return f'''For the given statement, the system response is : 
-        'anger': {emotions['anger']}, 
-        'disgust': {emotions['disgust']}, 
-        'fear': {emotions['fear']}, 
-        'joy': {emotions['joy']} and 
-        'sadness': {emotions['sadness']}. 
-        The dominant emotions is <strong> {emotions['dominant_emotion']} </strong>.'''
-
-
+    return f"""For the given statement, the system response is
+    'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, 
+    'fear': {emotions['fear']}, 'joy': {emotions['joy']} and 'sadness': {emotions['sadness']}.
+    The dominant emotion is <strong>{emotions['dominant_emotion']}</strong>"""
 
 @app.route("/")
 def render_index_page():
-    return render_template('index.html')
+    """
+    Renders the index page.
+    """
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5000)
+    app.run(host="0.0.0.0", port=5000)
